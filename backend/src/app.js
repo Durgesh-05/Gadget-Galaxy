b975c0924b484c09760379b8282a911c5ce042ac
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-09-29 20:28:24 +0530

    Update CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['https://gadget-galaxy-psi.vercel.app', 'http://localhost:5173'],
+    origin: 'https://gadget-galaxy-psi.vercel.app',
     credentials: true,
   })
 );
